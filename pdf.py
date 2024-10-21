18af0507ff4a8f5c2fff18fd4461350c6504423e
Alex Sebastian <39462592+alexsebastian56@users.noreply.github.com>
2024-10-21 09:41:27 -0700

    Added files needed for running the AI Agent

---
+++ pdf.py
@@ -0,0 +1,28 @@
+#vector store index
+
+import os
+from llama_index.core import StorageContext, VectorStoreIndex, load_index_from_storage
+from llama_index.readers.smart_pdf_loader import SmartPDFLoader
+
+def get_index(data, index_name):
+    index = None
+    if not os.path.exists(index_name):
+        print("building index", index_name)
+        index = VectorStoreIndex.from_documents(data, show_progress=True)
+        index.storage_context.persist(persist_dir=index_name)
+    else:
+        index = load_index_from_storage(
+            StorageContext.from_defaults(persist_dir=index_name)
+        )
+
+    return index
+
+pdf_path = os.path.join("data", "canada.pdf")
+canada_pdf = SmartPDFLoader().load_data(file=pdf_path)
+canada_index = get_index(canada_pdf, "canada")
+canada_engine = canada_index.as_query_engine()
+
+
+
+
+
